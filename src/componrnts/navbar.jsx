8806c9ad95611181bd773d5ad095b7aab3524c58
AasthaAgrawal444 <113307194+AasthaAgrawal444@users.noreply.github.com>
2022-10-23 07:43:35 +0530

    Add files via upload

---
+++ src/componrnts/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './navbar.css';
+
+function Nav() {
+  return(
+    <nav>
+        <div className = "navbar">
+            <h1>Skill Edge</h1>
+        </div>
+    </nav>
+  )
+
+}
+ export default Nav;
